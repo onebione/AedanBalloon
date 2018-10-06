7439dee83d0817fc0f5cd9d840adf44f8367fa90
Mark Chen <mark.chen@dda.kyocera.com>
2018-10-05 18:07:45 -0700

    add background scene

---
+++ Assets/RepeatingBackground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+
+    public float speed;
+    public float endY;
+    public float startY;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+
+        if (transform.position.y <= endY)
+        {
+            Vector2 pos = new Vector2(transform.position.x, startY);
+            transform.position = pos;
+        }
+	}
+}
