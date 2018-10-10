1e3d8e7d7896760b23ddea3f4982a59ed99ad674
Mark Chen <mark.chen@dda.kyocera.com>
2018-10-09 17:48:24 -0700

    added arts folder, going for a obstacle scene.

---
+++ Assets/ObstacleDamageBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleDamageBehavior : MonoBehaviour {
+
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // If the entering collider is the player...
+        if (other.gameObject == player)
+        {
+            // ... the player is in range.
+            
+        }
+    }
+}
