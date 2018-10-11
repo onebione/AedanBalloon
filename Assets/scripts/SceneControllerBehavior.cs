e6784dde09a61a6bca96f211820e27b4b93edcac
Mark Chen <mark.chen@dda.kyocera.com>
2018-10-10 17:13:07 -0700

    adding dev progress gifs

---
+++ Assets/scripts/SceneControllerBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControllerBehavior : MonoBehaviour {
+    //use this script to control what goes on in a scene, programmatically. 
+    //this script might end up only good for one particular scene. but that's probably ok. otherwise you would 
+    //end up with too many layers. 
+    public GameObject Obstacle;
+    public float TimeBetweenObstacles;
+    float FixedTimeBetweenObstacles;
+    public float StartY;
+    public float StartX;
+    public float EndX; //randomly make an obstacle between two X's.  
+	// Use this for initialization
+	void Start () {
+        //just remember it for later reference use. 
+        FixedTimeBetweenObstacles = TimeBetweenObstacles;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(TimeBetweenObstacles);
+        TimeBetweenObstacles -= Time.deltaTime;
+        if (TimeBetweenObstacles <= 0)
+        {
+            var x = Random.Range(StartX, EndX);
+            var pos = new Vector3(x, StartY, 3);
+            Instantiate(Obstacle, pos, Quaternion.identity);
+            TimeBetweenObstacles = FixedTimeBetweenObstacles;
+        }
+    }
+}
