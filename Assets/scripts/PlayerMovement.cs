6094730145e39e2526f15903f9f4de3c319fb6bc
Mark Chen <mark.chen@dda.kyocera.com>
2018-10-15 17:22:27 -0700

    the progress gif

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public PlayerController controller;
+    public float speed = 10f;
+
+    float horizontalMove = 0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Input.GetAxisRaw("Horizontal"));
+        horizontalMove = Input.GetAxisRaw("Horizontal") * speed;//left arrow -1, right arrow 1. 
+
+    }
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+    }
+}
